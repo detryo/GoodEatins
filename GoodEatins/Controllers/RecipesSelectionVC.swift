73da76e9ae7599e3686d726cf691e11c0538cef3
Chris <chris.sedano.arenas@gmail.com>
2020-05-15 16:48:37 +0100

    CollectionView Protocols
    implement protocols for UICollectionView in RecipesSelectionVC, pass data and config all protocols

--- GoodEatins/Controllers/RecipesSelectionVC.swift
+++ GoodEatins/Controllers/RecipesSelectionVC.swift
@@ -12,13 +12,42 @@ class RecipesSelectionVC: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    
     var selectedCategory : String!
+    var recipes : [Recipe]!
+    let data = DataSet()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        recipes = data.getRecipes(forCategoryTitle: selectedCategory)
     }
 
+}
+
+extension RecipesSelectionVC : UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return recipes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecipeCell", for: indexPath) as? RecipeCell {
+            
+            let recipe = recipes[indexPath.item]
+            cell.configureCell(recipe: recipe)
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = view.bounds.width
+        let cellDimension = (width / 2) - 15
+        return CGSize(width: cellDimension, height: cellDimension)
+    }
 }
